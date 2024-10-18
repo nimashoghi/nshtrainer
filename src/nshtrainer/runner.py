c43589595b84a0770a4fb4b030a5c683ec49fed3
Nima Shoghi <nimashoghi@gmail.com>
2024-10-18 19:56:34 -0400

    Add backwards compat for the deprecated runner

--- src/nshtrainer/runner.py
+++ src/nshtrainer/runner.py
@@ -1,10 +1,10 @@
 import copy
-import functools
+import logging
 from collections.abc import Callable, Iterable, Mapping, Sequence
+from pathlib import Path
 from typing import Generic
 
-from nshrunner import RunInfo, Snapshot
-from nshrunner import Runner as _Runner
+import nshrunner as nr
 from nshrunner._submit import screen
 from typing_extensions import TypeVar, TypeVarTuple, Unpack, deprecated, override
 
@@ -17,33 +17,25 @@ TReturn = TypeVar("TReturn", infer_variance=True)
 
 @deprecated("Use nshrunner.Runner instead.")
 class Runner(
-    _Runner[TReturn, TConfig, Unpack[TArguments]],
+    nr.Runner[TReturn, TConfig, Unpack[TArguments]],
     Generic[TReturn, TConfig, Unpack[TArguments]],
 ):
     @override
-    @classmethod
-    def default_validate_fn(cls, config: TConfig, *args: Unpack[TArguments]) -> None:
-        super().default_validate_fn(config, *args)
-
-    @override
-    @classmethod
-    def default_info_fn(cls, config: TConfig, *args: Unpack[TArguments]) -> RunInfo:
-        run_info = super().default_info_fn(config, *args)
-        return {
-            **run_info,
-            "id": config.id,
-            "base_dir": config.directory.project_root,
-        }
-
-    def _fast_dev_run_transform(
+    def __init__(
         self,
-        config: TConfig,
-        *args: Unpack[TArguments],
-        n_batches: int,
+        run_fn: Callable[[TConfig, Unpack[TArguments]], TReturn],
+        config: nr.RunnerConfig | None = None,
     ):
-        config = copy.deepcopy(config)
-        config.trainer.fast_dev_run = n_batches
-        return (config, *args)
+        if config is None:
+            working_dir = Path.cwd() / "nshrunner"
+            working_dir.mkdir(exist_ok=True)
+
+            logging.warning(
+                f"`config` is not provided. Using default working directory of {working_dir}."
+            )
+            config = nr.RunnerConfig(working_dir=working_dir)
+
+        super().__init__(run_fn, config)
 
     def fast_dev_run(
         self,
@@ -51,20 +43,15 @@ class Runner(
         n_batches: int = 1,
         *,
         env: Mapping[str, str] | None = None,
-        transforms: list[
-            Callable[[TConfig, Unpack[TArguments]], tuple[TConfig, Unpack[TArguments]]]
-        ]
-        | None = None,
     ):
-        transforms = transforms or []
-        transforms.append(
-            functools.partial(self._fast_dev_run_transform, n_batches=n_batches)
-        )
-        return self.local(
-            runs,
-            env=env,
-            transforms=transforms,
-        )
+        runs_updated: list[tuple[TConfig, Unpack[TArguments]]] = []
+        for args in runs:
+            config = copy.deepcopy(args[0])
+            config.trainer.fast_dev_run = n_batches
+            runs_updated.append((config, *args[1:]))
+        del runs
+
+        return self.local(runs_updated, env=env)
 
     def fast_dev_run_generator(
         self,
@@ -72,20 +59,15 @@ class Runner(
         n_batches: int = 1,
         *,
         env: Mapping[str, str] | None = None,
-        transforms: list[
-            Callable[[TConfig, Unpack[TArguments]], tuple[TConfig, Unpack[TArguments]]]
-        ]
-        | None = None,
     ):
-        transforms = transforms or []
-        transforms.append(
-            functools.partial(self._fast_dev_run_transform, n_batches=n_batches)
-        )
-        return self.local_generator(
-            runs,
-            env=env,
-            transforms=transforms,
-        )
+        runs_updated: list[tuple[TConfig, Unpack[TArguments]]] = []
+        for args in runs:
+            config = copy.deepcopy(args[0])
+            config.trainer.fast_dev_run = n_batches
+            runs_updated.append((config, *args[1:]))
+        del runs
+
+        return self.local_generator(runs_updated, env=env)
 
     def fast_dev_run_session(
         self,
@@ -93,27 +75,25 @@ class Runner(
         options: screen.ScreenJobKwargs = {},
         n_batches: int = 1,
         *,
-        snapshot: Snapshot,
+        snapshot: nr.Snapshot,
         setup_commands: Sequence[str] | None = None,
         env: Mapping[str, str] | None = None,
-        transforms: list[
-            Callable[[TConfig, Unpack[TArguments]], tuple[TConfig, Unpack[TArguments]]]
-        ]
-        | None = None,
         activate_venv: bool = True,
         print_command: bool = True,
     ):
-        transforms = transforms or []
-        transforms.append(
-            functools.partial(self._fast_dev_run_transform, n_batches=n_batches)
-        )
+        runs_updated: list[tuple[TConfig, Unpack[TArguments]]] = []
+        for args in runs:
+            config = copy.deepcopy(args[0])
+            config.trainer.fast_dev_run = n_batches
+            runs_updated.append((config, *args[1:]))
+        del runs
+
         return self.session(
-            runs,
+            runs_updated,
             options,
             snapshot=snapshot,
             setup_commands=setup_commands,
             env=env,
-            transforms=transforms,
             activate_venv=activate_venv,
             print_command=print_command,
         )
