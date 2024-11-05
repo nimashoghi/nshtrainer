bae49ffb9af7e9527653bb8db35ef4f1c34e026d
Nima Shoghi <nimashoghi@gmail.com>
2024-11-05 12:14:44 -0500

    Heavily simplify config exports

--- src/nshtrainer/configs/profiler/pytorch/__init__.py
+++ src/nshtrainer/configs/profiler/pytorch/__init__.py
@@ -2,30 +2,5 @@ from __future__ import annotations
 
 __codegen__ = True
 
-from typing import TYPE_CHECKING
-
-# Config/alias imports
-
-if TYPE_CHECKING:
-    from nshtrainer.profiler.pytorch import BaseProfilerConfig as BaseProfilerConfig
-    from nshtrainer.profiler.pytorch import (
-        PyTorchProfilerConfig as PyTorchProfilerConfig,
-    )
-else:
-
-    def __getattr__(name):
-        import importlib
-
-        if name in globals():
-            return globals()[name]
-        if name == "BaseProfilerConfig":
-            return importlib.import_module(
-                "nshtrainer.profiler.pytorch"
-            ).BaseProfilerConfig
-        if name == "PyTorchProfilerConfig":
-            return importlib.import_module(
-                "nshtrainer.profiler.pytorch"
-            ).PyTorchProfilerConfig
-        raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
-
-# Submodule exports
+from nshtrainer.profiler.pytorch import BaseProfilerConfig as BaseProfilerConfig
+from nshtrainer.profiler.pytorch import PyTorchProfilerConfig as PyTorchProfilerConfig
