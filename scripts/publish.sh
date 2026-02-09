10e3f4de0f082424cacfc8e4622ac112ea9c77fa
Nima Shoghi <nimashoghi@gmail.com>
2026-02-09 02:14:07 +0000

    feat(scripts): add publish script

---
+++ scripts/publish.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+set -u
+
+# 0. Make sure all dependencies are installed
+uv sync --all-extras --all-groups
+
+# 1. Run checks (loading .env if your tests need it)
+uv run --env-file .env ruff check
+uv run --env-file .env basedpyright
+uv run --env-file .env pytest
+
+# 2. Build and Publish
+uv build --clear
+uv run --env-file .env uv publish $@
